326a3e6c5cc95c90c01230c391da5ac8ab8dc77d
Alberto97 <albertop2197@gmail.com>
2017-08-22 13:41:30 +0200

    addison: don't build CMActions for now

--- device.mk
+++ device.mk
@@ -102,8 +102,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/ov16860_chromatix.xml:system/etc/camera/ov16860_chromatix.xml
 
 # CMActions
-PRODUCT_PACKAGES += \
-    CMActions
+#PRODUCT_PACKAGES += \
+#    CMActions
 
 # Display
 PRODUCT_PACKAGES += \
